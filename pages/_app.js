2cedf9f46f88c077a82a3cd25dcaf13791747669
EkaterinaYurchyk2022 <ekaterina.yurchyk.s@gmail.com>
2023-06-19 23:38:22 +0300

    init: initial commit

---
+++ pages/_app.js
@@ -0,0 +1,9 @@
+import Layout from "@/components/common/Layout";
+
+export default function App({Component, pageProps}) {
+    return(
+        <Layout>
+            <Component {...pageProps} />
+        </Layout>
+        )
+}
